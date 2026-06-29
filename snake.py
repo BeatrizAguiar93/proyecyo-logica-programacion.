9f10bd98b9d5d79b737cf6a69786a94eb2dbd7ac
BeatrizAguiar93 <ifififceleste_94@hotmail.com>
2026-06-28 21:02:38 -0500

    Add files via upload

--- snake.py
+++ snake.py
@@ -1,14 +1,32 @@
 #juego_serpiente
+#IMPORTACION DE VARIBLES
+
+import random
+import sys 
+# DECLARACION DE VARIABLES
 
 x = 5
 y = 5
+foot_x = (random.randint(0,10))
+foot_y = (random.randint(0,10))
+
+#INTRUCCIONES
 
 print("inicio")
 print("usa: w:arriba s:abajo a:izquierda d:abajo")
 
+# INICIO DEL JUEGO
+
 while True:
     print(f"\nposicion actual: ({x}),{y}")
     direccion=input("direccion:")
+# CREACION DE COMIDA
+    
+    if x == food_x and y == food_y:
+        food_x = random.randint(0, 9)
+        food_y = random.randint(0, 9)
+        
+#
 
     if direccion == "w":
         y+= 1
